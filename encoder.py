fa1106bb32e1fd8b3c49305d7956304e892ce04e
arwendowers <127459621+arwendowers@users.noreply.github.com>
2023-03-10 21:38:59 -0500

    Add files via upload

---
+++ encoder.py
@@ -0,0 +1,35 @@
+# Arwen Dowers
+
+def encode(password):                         # encodes inputted password
+    encoded_password = ''                     # starts as an empty string
+    for digit in password:                    # goes through each number of password
+        new_digit = int(digit) + 3            # adds 3 to each digit
+        if new_digit > 9:                     # if new digit is two digits,
+            new_digit -= 10                   # subtracts 10 to roll digit over into one digit
+        encoded_password += str(new_digit)    # adds new digits to encoded password string
+    return encoded_password
+
+
+def main():
+    option = 0
+    password = 'none'  # prints 'none' statements if no password has been inputted when decoding
+    encoded_password = 'none'
+    while option != 3:  # option 3 is Quit
+        print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit')
+        print()
+        option = int(input('Please enter an option: '))
+        while 1 <= option <= 2:
+            if option == 1:  # Encode
+                password = input('Please enter your password to encode: ')  # user inputs password
+                encoded_password = encode(password)  # calls encode function, then stored in variable
+                print('Your password has been encoded and stored!')
+                print()
+                break
+            if option == 2:  # Decode
+                print(f'The encoded password is {encoded_password}, and the original password is {password}.')
+                print()
+                break
+
+
+if __name__ == '__main__':
+    main()
